c655a7b2752dcd4a149271b3c161ffbd14ae5648
batook <batook@mail.ru>
2017-05-17 19:03:09 +0300

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,6 +9,10 @@ public class Test {
     private int a;
 
     public static void main(String[] args) {
+        Runtime rt = Runtime.getRuntime();
+        System.out.println("Total " + rt.totalMemory() / 1024 / 1024 + "MB");
+        System.out.println("Free " + rt.freeMemory() / 1024 / 1024 + "MB");
+        System.out.println();
         Calendar Cal = Calendar.getInstance();
         System.out.println(Cal.get(Cal.YEAR));
         int b = 10;
@@ -19,10 +23,24 @@ public class Test {
             reverseString += s.charAt(i);
         }
         System.out.println(reverseString);
-
+        System.out.println(new StringBuilder("abcd").reverse());
         B cb = new B();
         cb.addAll(3);
         System.out.println(cb.getCnt());
+        System.out.println();
+
+        A1 a1 = new A1();
+        System.out.println();
+        A1 a2 = new A2();
+        System.out.println();
+        A1 a3 = new A3();
+        System.out.println();
+
+        String s1 = "Abc";
+        String s2 = s1;
+        s2.toUpperCase();
+        System.out.println(s1);
+        System.out.println(s2);
     }
 
     public void go(Long l) {
@@ -32,14 +50,14 @@ public class Test {
 
 class A {
     public void add(int i) {
-        System.out.println(this+" A.add="+i);
+        System.out.println(this + " A.add=" + i);
     }
 
     public void addAll(int t) {
-        System.out.println(this+" A.addAll="+t);
+        System.out.println(this + " A.addAll=" + t);
         //for (int i = 0; i < t; i++) {
-            add(1);
-       // }
+        add(1);
+        // }
     }
 }
 
@@ -48,19 +66,54 @@ class B extends A {
 
     @Override
     public void add(int t) {
-        System.out.println(this+" B.add="+t);
+        System.out.println(this + " B.add=" + t);
         cnt = cnt + t;
         super.add(t);
     }
 
     @Override
     public void addAll(int t) {
-        System.out.println(this+" B.addAll="+t);
+        System.out.println(this + " B.addAll=" + t);
         cnt = cnt + t;
         super.addAll(t);
     }
 
     public int getCnt() {
         return cnt;
+    }
+}
+
+
+class A1 {
+    {
+        System.out.println("init1 A1");
+    }
+
+    {
+        System.out.println("init2 A1");
+    }
+
+    A1() {
+        System.out.println("A1");
+    }
+}
+
+class A2 extends A1 {
+    {
+        System.out.println("init A2");
+    }
+
+    A2() {
+        System.out.println("A2");
+    }
+}
+
+class A3 extends A2 {
+    {
+        System.out.println("init A3");
+    }
+
+    A3() {
+        System.out.println("A3");
     }
 }
